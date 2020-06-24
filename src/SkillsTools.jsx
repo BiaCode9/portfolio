87d5223e827b5b4b5ecfd8987e655591893aee68
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-24 19:35:47 -0400

    updated skills tools set to postman

--- src/SkillsTools.jsx
+++ src/SkillsTools.jsx
@@ -18,9 +18,10 @@ class SkillsTools extends Component {
             <li><img className="icon-react" src='https://i.imgur.com/B1t5sLV.png'></img></li>
             <li><img className="icon-node" src='https://i.imgur.com/4QHOWz0.png'></img></li>
             <li><img className="icon-ruby" src='https://i.imgur.com/fPT9y93.png'></img></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><img className="icon-rails" src='https://i.imgur.com/izjAdkv.png'></img></li>
+            <li><img className="icon-sql" src='https://i.imgur.com/ybyrJOX.png'></img></li>
+            <li><img className="icon-mongo" src='https://i.imgur.com/kxRpYHA.jpg'></img></li>
+            <li><img className="icon-postman" src='https://i.imgur.com/7uyzkv8.png'></img></li>
 
 
 
