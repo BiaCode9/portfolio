d89419e25bad2cc7c7b9bcde092b38d032a77e38
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-06-26 13:50:03 -0400

    adjusted css projects

--- src/Project.js
+++ src/Project.js
@@ -4,9 +4,11 @@ import './Project.css';
 
 class Project extends Component {
   render() {
+
     return (
+
       <section className="all-projects">
-        <h1 className="projects">Projects</h1>
+
         <div className="mm-project-container">
 
           <div className="title">
@@ -16,10 +18,12 @@ class Project extends Component {
             <img className='project-img' src='https://i.imgur.com/gIFT77p.png'></img>
           </div>
           <div className='project-description'>
-            <p>'A Baltimore detective and a medical examiner must work together when the witnesses of a long-ago crime become the victims of a deranged killer in this gripping romantic suspense novel.'</p>
+            <p>Music Triva Mayhem is a music trivia website that challenges the user with interesting and unknown facts about the music industry.</p>
           </div>
-          <button>Deployed Project</button>
-          <button>Project Github</button>
+          <div>
+            <button>Project URL</button>
+            <button>Project Github</button>
+          </div>
         </div>
 
 
@@ -31,7 +35,11 @@ class Project extends Component {
             <img className='project-img' src='https://i.imgur.com/gIFT77p.png'></img>
           </div>
           <div className='project-description'>
-            <p>';aklsjdf;laskdjfal;skdjfal;skdjfals;dkjfals;dkfjsal;dkjf'</p>
+            <p>InstaBook is a real-time job post / search platform for last-minute job openings. Users who are online or signed-up will get real-time job alerts for those posts that match their job description profile.</p>
+          </div>
+          <div>
+            <button>Project URL</button>
+            <button>Project Github</button>
           </div>
         </div>
 
@@ -46,20 +54,17 @@ class Project extends Component {
             <img className='project-img' src='https://i.imgur.com/gIFT77p.png'></img>
           </div>
           <div className='project-description'>
-            <p>'99999999999999</p>
+            <p>'A reacreation of the Instacart-Costco website and mobile application. Instacart is a grocery delivery and pick-up service. Worked on a team consisting of 4 other software engineers and 3 user experience (UX/UI)designers.</p>
+          </div>
+          <div>
+            <button>Project URL</button>
+            <button>Project Github</button>
           </div>
         </div>
 
       </section>
 
-
-
-
-
-
-
-
-      // <div className="project-title">
+      /* // <div className="project-title">
       //   <h2>{this.props.title}</h2>
       // </div>
       // <div className='project-img'>
@@ -69,7 +74,7 @@ class Project extends Component {
       //   <p>{this.props.description}</p>
       // </div>
       //   </div >
-
+ */
 
 
 
