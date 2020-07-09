3db448ac9a79d130106e9aee2443d7b2646f031e
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-07-09 15:36:17 -0400

    adjust css to jump to sections

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -5,26 +5,26 @@ import "./AboutMe.css"
 class AboutMe extends Component {
   render() {
     return (
-      <container>
-        <div className="about-me-container">
-          <div className="name">
-            <a id="about"></a>
-            <h2>Hi, <span>I'm Bianca!</span></h2>
-          </div>
-          <div className="title">
-            <h2>Software Engineer / Full Stack Developer</h2>
-          </div>
-          <div>
-            <img className='self-img' alt="personal pic" src='https://i.imgur.com/pzPY0IN.png'></img>
-          </div>
-          <div className="description">
-            <p>I am a highly-motivated full-stack web developer with an extensive background in music and film production, management, marketing and design.  My passion for creativity and technology combined with my problem-solving mind-set fuels my drive as a software engineer.</p>
-            <p><span>I'm always up for a challenge and enjoy working with others and independently as needed seeing projects through to their full fruition!</span></p>
-          </div>
+
+      <div className="about-me-container">
+        <div className="name">
+          <a id="about"></a>
+          <h2>Hi, <span>I'm Bianca!</span></h2>
         </div>
+        <div className="title">
+          <h2>Software Engineer / Full Stack Developer</h2>
+        </div>
+        <div>
+          <img className='self-img' alt="personal pic" src='https://i.imgur.com/pzPY0IN.png'></img>
+        </div>
+        <div className="description">
+          <p>I am a highly-motivated full-stack web developer with an extensive background in music and film production, management, marketing and design.  My passion for creativity and technology combined with my problem-solving mind-set fuels my drive as a software engineer.</p>
+          <p><span>I'm always up for a challenge and enjoy working with others and independently as needed seeing projects through to their full fruition!</span></p>
+        </div>
+      </div>
 
 
-      </container>
+
 
     )
   }
