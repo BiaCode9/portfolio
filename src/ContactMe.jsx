ce0e0ab5a75636d913c0f46a479eeca799d6fc29
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-07-09 19:34:25 -0400

    added target blanks for contact section

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -16,22 +16,21 @@ class ContactMe extends Component {
             <p> <span className="contact-me-span"> Contact Me</span><p></p><span className="anytime-span">Anytime, Anywhere</span> </p>
             <img className="pin-drop" alt="pin-drop" src="https://i.imgur.com/iBeuWht.png"></img>
             <p><span className="get-back-span">I will get back to you</span> <span className="asap-span"> as soon as possible!</span></p>
-
           </div>
 
           <div>
             <ul className="contact-box">
               <li>
-                <a href="mailto:biacode2020@gmail.com"><img className="icon-email" alt="icon-email" src="https://i.imgur.com/qorTRFp.png"></img></a>
+                <a href="mailto:biacode2020@gmail.com" target="_blank"><img className="icon-email" alt="icon-email" src="https://i.imgur.com/qorTRFp.png"></img></a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/biancabiagioni/"><img className="icon-linkedin" alt="icon-linkedin" src="https://i.imgur.com/ySrvm6e.png"></img></a>
+                <a href="https://www.linkedin.com/in/biancabiagioni/" target="_blank"><img className="icon-linkedin" alt="icon-linkedin" src="https://i.imgur.com/ySrvm6e.png"></img></a>
               </li>
               <li>
-                <a href="https://github.com/BiaCode9"><img className="icon-github" alt="icon-github" src="https://i.imgur.com/xSnqBXy.png"></img></a>
+                <a href="https://github.com/BiaCode9" target="_blank"><img className="icon-github" alt="icon-github" src="https://i.imgur.com/xSnqBXy.png"></img></a>
               </li>
               <li>
-                <a href="https://docdro.id/QGc2HAm"><img className="icon-resume" alt="icon-resume" src="https://i.imgur.com/tGjhB2E.png"></img></a>
+                <a href="https://docdro.id/QGc2HAm" target="_blank"><img className="icon-resume" alt="icon-resume" src="https://i.imgur.com/tGjhB2E.png"></img></a>
               </li>
             </ul>
           </div>
