e7bd2e7f70566e0ece609529b0bb25cf03865711
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-07-14 15:30:23 -0400

    cleaned up project code

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -16,23 +16,23 @@ class ContactMe extends Component {
           </div>
           <div className="contact-please">
             <p> <span className="contact-me-span">CONTACT ME</span><p></p><span className="anytime-span">Anytime, Anywhere</span> </p>
-            <img className="pin-drop" alt="pin-drop" src="https://i.imgur.com/iBeuWht.png"></img>
+            <img className="pin-drop" alt="pin-drop" src="https://i.imgur.com/iBeuWht.png" />
             <p><span className="get-back-span">I will get back to you</span> <span className="asap-span"> as soon as possible!</span></p>
           </div>
 
           <div>
             <ul className="contact-box">
               <li>
-                <a href="mailto:biacode2020@gmail.com" target="_blank"><img className="icon-email" alt="icon-email" src="https://i.imgur.com/qorTRFp.png"></img></a>
+                <a href="mailto:biacode2020@gmail.com" target="_blank"><img className="icon-email" alt="icon-email" src="https://i.imgur.com/qorTRFp.png" /></a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/biancabiagioni/" target="_blank"><img className="icon-linkedin" alt="icon-linkedin" src="https://i.imgur.com/ySrvm6e.png"></img></a>
+                <a href="https://www.linkedin.com/in/biancabiagioni/" target="_blank"><img className="icon-linkedin" alt="icon-linkedin" src="https://i.imgur.com/ySrvm6e.png" /></a>
               </li>
               <li>
-                <a href="https://github.com/BiaCode9" target="_blank"><img className="icon-github" alt="icon-github" src="https://i.imgur.com/xSnqBXy.png"></img></a>
+                <a href="https://github.com/BiaCode9" target="_blank"><img className="icon-github" alt="icon-github" src="https://i.imgur.com/xSnqBXy.png" /></a>
               </li>
               <li>
-                <a href="https://docdro.id/QGc2HAm" target="_blank"><img className="icon-resume" alt="icon-resume" src="https://i.imgur.com/tGjhB2E.png"></img></a>
+                <a href="https://docdro.id/QGc2HAm" target="_blank"><img className="icon-resume" alt="icon-resume" src="https://i.imgur.com/tGjhB2E.png" /></a>
               </li>
             </ul>
           </div>
