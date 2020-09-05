3ea31893374134e31601ec15ef3fc4c9d0889ed2
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-09-05 02:28:35 -0400

    updated contact box

--- src/SkillsTools.jsx
+++ src/SkillsTools.jsx
@@ -25,8 +25,9 @@ class SkillsTools extends Component {
             <li>POSTMAN<img className="icon-postman" alt="icon-postman" src='https://i.imgur.com/7uyzkv8.png'></img></li>
             <li>EXPRESS<img className="icon-express" alt="icon-express" src='https://i.imgur.com/0h3DBpx.png'></img></li>
             <li>STORYBOOK<img className="icon-storybook" alt="icon-storybook" src='https://i.imgur.com/3wTjDri.png'></img></li>
-            <li>PHOTOSHOP<img className="icon-photoshop" alt="icon-photoshop" src='https://i.imgur.com/spVNuCw.png'></img></li>
-            <li>CREATIVE CLOUD<img className="icon-adobe" alt="icon-adobe" src='https://i.imgur.com/G367NGy.png'></img></li>
+            <li>FIGMA<img className="icon-figma" alt="icon-figma" src='https://i.imgur.com/ld16pHL.png'></img></li>
+            {/* <li>PHOTOSHOP<img className="icon-photoshop" alt="icon-photoshop" src='https://i.imgur.com/spVNuCw.png'></img></li> */}
+            <li>ADOBE CREATIVE CLOUD<img className="icon-adobe" alt="icon-adobe" src='https://i.imgur.com/G367NGy.png'></img></li>
             <li>HEROKU<img className="icon-heroku" alt="icon-heroku" src='https://i.imgur.com/0fFRcPg.png'></img></li>
             <li>NETLIFY<img className="icon-netlify" alt="icon-netlify" src='https://i.imgur.com/apSr06z.jpg'></img></li>
             <li>SURGE<img className="icon-surge" alt="icon-surge" src='https://i.imgur.com/PCk4lh6.jpg'></img></li>
