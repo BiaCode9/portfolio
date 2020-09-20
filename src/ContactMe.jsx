ec7f9fe5c3af1ba3d6dbca66d61f30ff61d60298
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-09-20 14:02:31 -0400

    fixed contact box

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -1,13 +1,9 @@
-
-import React, { Component } from "react"
-import './ContactMe.css';
+import React, { Component } from "react";
+import "./ContactMe.css";
 
 class ContactMe extends Component {
-
-
   render() {
     return (
-
       <div className="contact-container">
         <a id="contact"></a>
         <div className="contact-form">
@@ -16,51 +12,81 @@ class ContactMe extends Component {
           </div>
           <div className="contact-please"> */}
           <div className="contact-aa">
-            <p><h1 className="contact-me-span">CONTACT ME</h1></p><p><span className="anytime">Anytime</span><span className="anywhere-span">Anywhere</span></p>
-            <img className="pin-drop" alt="pin-drop" src="https://i.imgur.com/Q3iqEHG.png" />
+            <p>
+              <h1 className="contact-me-span">CONTACT ME</h1>
+            </p>
+            <p>
+              <span className="anytime">Anytime</span>
+              <span className="anywhere-span">Anywhere</span>
+            </p>
+            <img
+              className="pin-drop"
+              alt="pin-drop"
+              src="https://i.imgur.com/Q3iqEHG.png"
+            />
             {/* <img src-="https://i.imgur.com/8LA4tO7.jpg"/> */}
             <div className="contact-box">
-              {/* <ul> */}
+              <ul className="list">
 
               <li className="link-list">
-                <a href="mailto:biacode2020@gmail.com" target="_blank"><img className="icon-email" alt="icon-email" src="https://i.imgur.com/qorTRFp.png" /></a>
+                <a href="mailto:info@biabia.dev" target="_blank">
+                  <img
+                    className="icon-email"
+                    alt="icon-email"
+                    src="https://i.imgur.com/qorTRFp.png"
+                  />
+                </a>
               </li>
               <li className="link-list">
-                <a href="https://www.linkedin.com/in/biancabiagioni/" target="_blank"><img className="icon-linkedin" alt="icon-linkedin" src="https://i.imgur.com/ySrvm6e.png" /></a>
+                <a
+                  href="https://www.linkedin.com/in/biancabiagioni/"
+                  target="_blank"
+                >
+                  <img
+                    className="icon-linkedin"
+                    alt="icon-linkedin"
+                    src="https://i.imgur.com/ySrvm6e.png"
+                  />
+                </a>
               </li>
               <li className="link-list">
-                <a href="https://github.com/BiaCode9" target="_blank"><img className="icon-github" alt="icon-github" src="https://i.imgur.com/xSnqBXy.png" /></a>
+                <a href="https://github.com/BiaCode9" target="_blank">
+                  <img
+                    className="icon-github"
+                    alt="icon-github"
+                    src="https://i.imgur.com/xSnqBXy.png"
+                  />
+                </a>
               </li>
               <li className="link-list">
-                <a href="tel:3107219600" target="_blank"><img className="icon-tele" alt="icon-tele" src="https://i.imgur.com/wFAd9W5.png" /></a>
+                <a href="tel:9172677597" target="_blank">
+                  <img
+                    className="icon-tele"
+                    alt="icon-tele"
+                    src="https://i.imgur.com/wFAd9W5.png"
+                  />
+                </a>
               </li>
               <li className="link-list">
-                <a href="https://drive.google.com/file/d/1q1tHXVl-Q84gRv7CxHYPWra1BLlTx9OH/view?usp=sharing" target="_blank"><img className="icon-resume" alt="icon-resume" src="https://i.imgur.com/tGjhB2E.png" /></a>
+                <a
+                  href="https://drive.google.com/file/d/1q1tHXVl-Q84gRv7CxHYPWra1BLlTx9OH/view?usp=sharing"
+                  target="_blank"
+                >
+                  <img
+                    className="icon-resume"
+                    alt="icon-resume"
+                    src="https://i.imgur.com/tGjhB2E.png"
+                  />
+                </a>
               </li>
-              {/* </ul> */}
+              </ul>
             </div>
 
-
-
-
-
-
             {/* <div className="blinklight"></div> */}
-
-
           </div>
         </div>
-
-
-
-
-
-
-
       </div>
-
-
-    )
+    );
   }
 }
-export default ContactMe
+export default ContactMe;
