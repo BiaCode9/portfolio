28544996cf5ee0b0cd20173295d2300ff87bced1
BiancaBiagioni <biacode2020@gmail.com>
2023-01-17 14:47:13 -0500

    updated phone number

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -58,7 +58,7 @@ class ContactMe extends Component {
                   </a>
                 </li>
                 <li className="link-list">
-                  <a href="tel:9174566454" target="_blank">
+                  <a href="tel:9177308830" target="_blank">
                     <img
                       className="icon-tele"
                       alt="icon-tele"
