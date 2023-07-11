5bfd7a7e2afe94e2561e55cfd1a3dc9f52982705
BiancaBiagioni <biacode2020@gmail.com>
2023-07-11 13:18:12 -0400

    udpated bio

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -18,9 +18,9 @@ class AboutMe extends Component {
           <img className='self-img' alt="personal pic" src='https://i.imgur.com/XE1UY5E.jpg'></img>
         </div>
         <div className="description">
-          <p>I am a highly-motivated full-stack web developer with an extensive background in music and film production, management, pr and design.  My passion for creativity and technology combined with my problem-solving mindset fuels my drive as a software engineer.</p>
-          <p><span>In addition to website design and development, I provide digital marketing services including social media management, content and blog writing, graphic design, hosting and maintenance, Search Engine Optimization (SEO), Google Business setup, review capture and management, and e-mail/text marketing campaigns.  </span></p>
-          <p>I'm always up for a challenge, gaurantee 100% customer satisfaction, and enjoy working with others or independently as needed seeing projects through to their full fruition!</p>
+          <p>I am a highly-motivated full-stack web developer with an extensive background in content design, music and film production, management, and PR.  My passion for creativity and technology combined with my problem-solving mindset fuels my drive for challenge and opportunity.</p>
+          <p><span>I also provide digital marketing services and packages including social media management, blogs, graphic design, news publications, website hosting and maintenance, Search Engine Optimization (SEO), review management, and e-mail/text campaigns.  </span></p>
+          <p>I gaurantee nothing less than 100% customer satisfaction and enjoy working with others or independently as needed seeing projects through to their full fruition!</p>
         </div>
       </div>
 
